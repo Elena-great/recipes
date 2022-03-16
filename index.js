7716a68f5ecca6112d04570fc534bb06f31e42c9
Elena-great <77853789+Elena-great@users.noreply.github.com>
2022-03-16 16:27:18 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const searchInput = document.querySelector('#search');
+const divs = document.querySelectorAll('.recipe');
+
+
+searchInput.addEventListener('keyup', function(e){
+  const word = e.target.value.toLowerCase();
+
+  divs.forEach(item => {
+    item.querySelector('p').textContent.toLowerCase().includes(word) ? (item.style.display = 'block') : (item.style.display = 'none');
+  })
+
+})
